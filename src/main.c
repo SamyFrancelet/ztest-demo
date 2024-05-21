c430f17cb7f7945baa2f9987346aa7cb80d5fa9f
Samy Francelet <samy@griffith.local>
2024-05-21 23:03:34 +0200

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <zephyr/kernel.h>
+#include <app_version.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(main);
+
+int main(void)
+{
+    LOG_INF("ZTEST Demo application, %s", APP_VERSION_STRING);
+
+    while (1) {
+        LOG_INF("Hello world !");
+        k_sleep(K_MSEC(100));
+    }
+
+    return 0;
+}
